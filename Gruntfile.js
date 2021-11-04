de976daea8aa14eff7454eccaab1aa94e9385201
zerg41 <zerg41@gmail.com>
2021-11-04 11:26:43 +0300

    dev: grunt part 1
    -installed grunt and grunt-cli (globally)
    -created a grunt file
    -installed additionally node-sass
    -configured grunt file for: scss to css convertion, watching and serving local website

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+    const sass = require('node-sass');
+
+    grunt.initConfig ({
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true,
+            },
+            dist: {
+                files: {
+                    'css/styles-sass.css': 'css/styles-sass.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ['css/*.css', '*.html', 'js/*.js']
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
