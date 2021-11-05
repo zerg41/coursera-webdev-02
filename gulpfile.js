43c1e368ca80da9b5a3d1522f9490ea13abf2ece
zerg41 <zerg41@gmail.com>
2021-11-05 16:38:52 +0300

    dev: gulp part 2
    -installed node modules:
    del (easy deleting),
    gulp-imagemin, gulp-usemin, gulp-htmlmin, gulp-clean-css, gulp-flatmap, gulp-rev, uglify (minifying)
    -fixed some issues with gulp depreciated syntax
    -added build script in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var browserSync = require('browser-sync');
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function() {
     return gulp.src('./css/*.scss')
@@ -29,8 +37,39 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// gulp.task('default', ['browser-sync'], function() {
-//     gulp.start('sass:watch');
-// });
+gulp.task('clean', function() {
+    return del(['dist']);
+});
 
+gulp.task('copyfonts', function() {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({
+        optimizationLevel: 3,
+        progressive: true,
+        interlaced: true}))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+          .pipe(usemin({
+              css: [ rev() ],
+              html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+              js: [ uglify(), rev() ],
+              inlinejs: [ uglify() ],
+              inlinecss: [ cleanCss(), 'concat' ]
+            }))
+        }))
+    .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copyfonts','imagemin','usemin')));
 gulp.task('default', gulp.parallel('browser-sync', 'sass:watch'));
