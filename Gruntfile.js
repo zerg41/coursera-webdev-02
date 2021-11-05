7f4ea4e10ee15634e4a67e3663596b94819de7ab
zerg41 <zerg41@gmail.com>
2021-11-05 10:21:05 +0300

    dev: grunt part 2
    -installed and setuped grunt modules:
    grunt-contrib-copy, grunt-contrib-clean (cleaning dist directory);
    grunt-contrib-htmlmin, grunt-contrib-cssmin, grunt-contrib-imagemin, grunt-contrib-usemin grunt-contrib-uglify grunt-contrib-usemin (preparing and minifying);
    -updated grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,10 @@
 module.exports = function(grunt) {
 
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
-    const sass = require('node-sass');
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin' // Static mapping используется для объявления плагинов вручную
+    });
+    const sass = require('node-sass'); // Указываем какой плагин использовать для задачи Sass
 
     grunt.initConfig ({
         sass: {
@@ -34,9 +36,112 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true, // Enable dynamic expansion
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'], // Actual patterns to match
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html, contact.html, about.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {} // This configuration is provided by useminPrepare
+        },
+        uglify: {
+            dist: {} // This configuration is provided by useminPrepare
+        },
+        cssmin: {
+            dist: {} // This configuration is provided by useminPrepare
+        },
+        usemin: {
+            html: ['dist/index.html, dist/contact.html, dist/about.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/about.html': 'dist/about.html',
+                    'dist/contact.html': 'dist/contact.html'
+                }
+            }
         }
     });
 
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+                                'clean', 
+                                'copy', 
+                                'imagemin',
+                                'useminPrepare',
+                                'concat',
+                                'cssmin',
+                                'uglify',
+                                'usemin',
+                                'htmlmin'
+                                ]
+    );
+
 };
