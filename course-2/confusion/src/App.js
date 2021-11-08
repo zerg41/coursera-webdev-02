bbf9124a111f94bc291a4bbe7b406cfa6e2015a2
zerg41 <zerg41@gmail.com>
2021-11-08 14:54:44 +0300

    dev: components part 2
    -changed app component from functional type into class type
    -replaced dish list from Menu-component into separate .js file (shared/dishes.js)
    -changed Media-component in MenuComponent into Card-component

--- course-2/confusion/src/App.js
+++ course-2/confusion/src/App.js
@@ -1,19 +1,33 @@
-// import logo from './logo.svg';
+// import logo from './logo.svg'; --no longer used
+import { Component } from 'react/cjs/react.production.min';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
-import './App.css';
+// import './App.css'; --no longer used
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color = "primary">
-        <div className = "container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar dark color = "primary">
+          <div className = "container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
