8fa61121524d37676f904d04f7677477af511a1c
zerg41 <zerg41@gmail.com>
2021-11-11 11:39:51 +0300

    dev: spa part 1
    -added new data files (leaders, features)
    -updated dishes file (comments moved into separate file)
    -integrated contact component
    -updated home component (feature cards were added)
    -updated MainComponent (props added to HomePage)

--- course-2/confusion/src/components/MainComponent.js
+++ course-2/confusion/src/components/MainComponent.js
@@ -3,9 +3,13 @@ import { Component } from 'react/cjs/react.production.min';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 class Main extends Component {
@@ -15,6 +19,9 @@ class Main extends Component {
 
     this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
             // selectedDish: null
         };
     }
@@ -28,7 +35,21 @@ class Main extends Component {
 
         const HomePage = () => {
             return(
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            );
+        }
+
+        // const ContactPage = () => {
+        //     return(
+        //         <Contact />
+        //     );
+        // }
+
+        const MenuPage = () => {
+            return(
+                <Menu dishes={this.state.dishes} />
             );
         }
 
@@ -45,13 +66,15 @@ class Main extends Component {
                           onClick={(dishId) => this.onDishSelect(dishId)} /> */}
                     {/* <DishDetail selectedDish={this.state.dishes
                                              .filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
-                                             
+
                     {/* Configuring the Router */}
                     <Switch>
                         {/* To routing without passing props */}
                         <Route path="/home" component={ HomePage } />
+                        <Route exact path="/contact" component={ Contact } />
                         {/* To routing with passing props */}
-                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+                        {/* <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } /> */}
+                        <Route exact path="/menu" component={ MenuPage } />
                         {/* Setting the default path */}
                         <Redirect to="/home" />
                     </Switch>
